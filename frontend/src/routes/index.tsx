751fd629157c2b49aaba9bd483a25c6f1f989356
Delmar Filho <ddlmar13@gmail.com>
2025-01-27 22:52:31 -0300

    feat: :sparkles: Create Table - Created new table based on shadcn

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,14 @@
-import { Button } from '@components/shadcn/ui/button'
+import Table from '@components/Table'
 import { api } from '@lib/api'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 const getBooks = async () => {
-  const response = await api.books.authors.$get()
+  const response = await api.books.$get()
 
   if (!response.ok) {
     throw new Error('Server error')
@@ -19,28 +18,26 @@ const getBooks = async () => {
 }
 
 function Index() {
-  const [count, setCount] = useState(0)
-
   const query = useQuery({
     queryKey: ['books'],
     queryFn: getBooks,
   })
 
   return (
-    <>
-      <div className="flex m-autos flex-col p-10 gap-4">
-        <Button
-          variant="secondary"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Upgrade
-        </Button>
-        <Button onClick={() => setCount((count) => count - 1)}>
-          Downgrade
-        </Button>
-        <p>{count}</p>
-        <code>{JSON.stringify(query.data, null, 2)}</code>
-      </div>
-    </>
+    <div className="flex m-autos flex-col p-10 gap-4">
+      <Table
+        caption="List of all books."
+        header={['id', 'name', 'author', 'date']}
+        fetchData={query.data?.books || []}
+        renderRow={(item, Cell) => (
+          <>
+            <Cell>{item.id}</Cell>
+            <Cell>{item.title}</Cell>
+            <Cell>{item.author}</Cell>
+            <Cell>{item.date}</Cell>
+          </>
+        )}
+      />
+    </div>
   )
 }
