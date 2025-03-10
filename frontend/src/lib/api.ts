02eced7f0a67a27ca479292d9cf6b4f04c7f813d
ddlmar <ddlmar13@gmail.com>
2025-03-10 20:06:05 -0300

    feat: :sparkles: Delete frontend

--- frontend/src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import { ApiRoutes } from '@server/app'
-
-import { hc } from 'hono/client'
-
-const client = hc<ApiRoutes>(`${import.meta.env.VITE_BASE_URL}`)
-
-export const api = client.api
--- frontend/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
-  component: Index,
-})
-
-function Index() {
-  return <div className="flex m-autos flex-col p-10 gap-4"></div>
-}
